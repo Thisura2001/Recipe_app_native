0697a10010b20fb8da46ab145064e40eba5f4a6c
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-02-26 09:31:40 +0530

    remove tab text and add icon to it

--- app/dashboard/_layout.tsx
+++ app/dashboard/_layout.tsx
@@ -1,14 +1,35 @@
-
-import {Provider} from "react-redux";
-import {store} from "../../Store/Store";
-import {Tabs} from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "../../Store/Store";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Dashboard() {
     return (
         <Provider store={store}>
-            <Tabs>
-                <Tabs.Screen name="dashboard" options={{title : "dashboard"}}/>
-                <Tabs.Screen name="SaveMeal" options={{title : "Saved meal"}}/>
+            <Tabs
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName: keyof typeof Ionicons.glyphMap;
+
+                        switch (route.name) {
+                            case "index":
+                                iconName = focused ? "home" : "home-outline";
+                                break;
+                            case "SaveMeal":
+                                iconName = focused ? "save" : "save-outline";
+                                break;
+                            default:
+                                iconName = "search";
+                        }
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: "blue",
+                    tabBarInactiveTintColor: "gray",
+                    tabBarShowLabel: false,
+                })}
+            >
+                <Tabs.Screen name="index" options={{ headerShown: false }} />
+                <Tabs.Screen name="SaveMeal" options={{ headerShown: false }} />
             </Tabs>
         </Provider>
     );
